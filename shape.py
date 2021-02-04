5b55a98a86f68a41af03f43f0dca0ab79826a944
SeongBaek-Cho <59674040+SeongBaek-Cho@users.noreply.github.com>
2021-02-05 00:27:32 +0900

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,15 +1,15 @@
 import os
 
-path = "Z:\\인포보스 부서별자료_사업개발실\\7.경상남도람사르환경재단_식물관리\\연안습지_SHP\\"
+
 file_list = os.listdir(path)
 count = 1
 for i in file_list :
     if (i.__contains__(".")) :
         print (i,"is files")
     else :
-        f = open("Z:\\인포보스 부서별자료_사업개발실\\7.경상남도람사르환경재단_식물관리\\연안습지_SHP\\%s\\shape.txt" % i, 'r', encoding='utf-8')
+        
         lines = f.readlines()
-        fp = "Z:\\인포보스 부서별자료_사업개발실\\7.경상남도람사르환경재단_식물관리\\연안습지_SHP\\"
+        
 
         cnt = 0
         init = None
